47bfaaf05a2588ae3dad649d01e24c051c2b8278
warugaki <warugaki.web.developer@gmail.com>
2023-02-12 14:05:07 +0900

    stylelintのルール変更
    - rscssをカスタマイズ

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -8,11 +8,15 @@ module.exports = {
   plugins: ['stylelint-rscss'],
   rules: {
     'selector-class-pattern': null,
-    'rscss/no-descendant-combinator': true,
+    'rscss/no-descendant-combinator': null,
     'rscss/class-format': [
       true,
       {
         // https://github.com/rstacruz/stylelint-rscss/blob/master/docs/rules.md#rscssclass-format
+        component: '^[a-z][a-zA-Z0-9]+$', // camelCase
+        element: '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$', // kebab-case
+        helper: '^_([a-z][a-z0-9]*)(-[a-z0-9]+)*$', //  underscore prefix and kebab-case
+        variant: '^-([a-z][a-z0-9]*)(-[a-z0-9]+)*$', // hyphen prefix and kebab-case
       },
     ],
   },
